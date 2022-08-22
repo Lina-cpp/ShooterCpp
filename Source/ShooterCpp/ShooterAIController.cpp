6f4e47447f20164759c1fd3cf9fe3e57377a5f03
Lina <107427736+Lina-cpp@users.noreply.github.com>
2022-08-22 11:46:10 +0200

    init

--- Source/ShooterCpp/ShooterAIController.cpp
+++ Source/ShooterCpp/ShooterAIController.cpp
@@ -3,3 +3,27 @@
 
 #include "ShooterAIController.h"
 
+#include "Kismet/GameplayStatics.h"
+
+void AShooterAIController::BeginPlay()
+{
+    Super::BeginPlay();
+    //focusing Ai on player
+    //first - get player pawn
+    APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+    //second - set focus
+    SetFocus(PlayerPawn);
+
+
+
+}
+
+void AShooterAIController::Tick(float DeltaSeconds)
+{
+    Super::Tick(DeltaSeconds);
+    
+    //get player pawn
+    APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+    //moving AI to player || get close to 2meters
+    MoveToActor(PlayerPawn, 200); 
+}
--- Source/ShooterCpp/ShooterAIController.h
+++ Source/ShooterCpp/ShooterAIController.h
@@ -13,5 +13,11 @@ UCLASS()
 class SHOOTERCPP_API AShooterAIController : public AAIController
 {
 	GENERATED_BODY()
+
+public:
+	virtual void Tick(float DeltaSeconds) override;
 	
+protected:
+	virtual void BeginPlay() override;
+
 };
